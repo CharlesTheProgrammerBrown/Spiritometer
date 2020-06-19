7602d860c7004b7f96200406ee3a2f80a66f8e49
CharlesTheProgrammer <charlie.tbrown7@gmail.com>
2020-06-19 14:16:55 +0300

    auth stream - user data

--- lib/Services/auth.dart
+++ lib/Services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:spiritometer/models/user.dart';
 //import 'package:flutter/material.dart';
 
 //define methods to interact with Firebase Auth
@@ -6,6 +7,28 @@ class AuthService{
   
 //instance of Firebase auth to communicate with firebase on backend
 final FirebaseAuth _auth = FirebaseAuth.instance;
+
+//create user obj based on FirebaseUser
+
+User _userFromFirebaseUser(FirebaseUser user){
+  return user!=null? User(uid:user.uid): null;
+}
+
+/*auth change user stream
+gets stream of FirebaseUser everytime users sign in
+Stream<FirebaseUser> get user{
+  return _auth.onAuthStateChanged;
+}
+*/
+
+//change type to User cos of map to normal user
+Stream<User> get user{
+  return _auth.onAuthStateChanged 
+  //get stream of Firebase User & return stream of normal user 
+  //.map((FirebaseUser user) => _userFromFirebaseUser(user));
+  .map(_userFromFirebaseUser);
+}
+ 
 
 //sign in with email and password
 Future signInEmail() async{
@@ -21,6 +44,22 @@ Future signInEmail() async{
   }
 }
 //register with email and password
+
+Future registerWithEmailAndPassword(String email, String password) async{
+try {
+  AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+
+//grab user from result
+FirebaseUser user = result.user;
+
+//turn FirebaseUser with multiple info to user class created wit jst ID
+return _userFromFirebaseUser(user);
+} catch (e) {
+  print(e.toString());
+  return null;
+}
+
+}
 
 //method to sign out
 }
