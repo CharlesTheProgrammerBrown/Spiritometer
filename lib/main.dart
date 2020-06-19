692e3a4bc818baf73b51db14cdf686451efa4566
CharlesTheProgrammer <charlie.tbrown7@gmail.com>
2020-06-19 14:17:26 +0300

    Provider Stream added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:spiritometer/RouteGenerator.dart';
+import 'package:provider/provider.dart';
+import 'package:spiritometer/Services/auth.dart';
+import 'package:spiritometer/models/user.dart';
 
 
 void main() => runApp(MyApp());
@@ -7,10 +10,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        initialRoute: '/', onGenerateRoute: RouteGenerator.generateRoute
-        //home: LoginScreen(),
-        );
+    return StreamProvider<User>.value(
+      //stream to listen 2 & expctd data
+          value:AuthService().user,
+          //pass on stream to all children widget
+          child: MaterialApp(
+          initialRoute: '/', onGenerateRoute: RouteGenerator.generateRoute
+          //home: LoginScreen(),
+          ),
+    );
   }
 }
 
