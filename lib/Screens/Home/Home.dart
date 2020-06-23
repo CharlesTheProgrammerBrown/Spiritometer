b5dc1c6f7c796fe2c1bde675de7c7c14ad166650
CharlesTheProgrammer <charlie.tbrown7@gmail.com>
2020-06-23 18:42:48 +0300

    app bar added to test signout

--- lib/Screens/Home/Home.dart
+++ lib/Screens/Home/Home.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:spiritometer/Services/auth.dart';
 import 'package:spiritometer/UI/MenuUI.dart';
 
 
 class Home extends StatelessWidget {
   // MainPage();
+final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar:AppBar(
+        //backgroundColor: Colors.,
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(onPressed: () async
+    {
+      await _auth.signOut();
+    }
+          , icon: Icon(Icons.person), label: Text('Sign Out'))
+        ],
+      ),
       body: Column(
         children: <Widget>[
           Stack(
@@ -15,7 +28,7 @@ class Home extends StatelessWidget {
             overflow: Overflow.visible,
             children: [
               Image(
-                  image: AssetImage('assets/images/Pst_Chris.jpg'),
+                  image: AssetImage('assets/images/pastor-chris.jpg'),
                   height: MediaQuery.of(context).size.height / 3,
                   width: double.infinity,
                   fit: BoxFit.cover),
@@ -26,7 +39,7 @@ class Home extends StatelessWidget {
                   backgroundColor: Color(0xFF398AE5), //Colors.deepPurpleAccent[400],
                   child: CircleAvatar(
                     radius: 72,
-                    backgroundImage: AssetImage('assets/images/CHARLES BROWN.jpg'),
+                    backgroundImage: AssetImage('assets/images/CHARLES_BROWN.jpg'),
                   ),
                 ),
               ),
