41367ec60a8c163ca3292e4075b27b77d94bc689
CharlesTheProgrammer <charlie.tbrown7@gmail.com>
2020-06-29 01:35:31 +0300

    slivers menu introduced

--- lib/Screens/Home/Home.dart
+++ lib/Screens/Home/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spiritometer/Screens/Home/menu.dart';
 import 'package:spiritometer/Services/auth.dart';
 import 'package:spiritometer/UI/MenuUI.dart';
 
@@ -10,57 +11,67 @@ final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        //backgroundColor: Colors.,
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(onPressed: () async
+      body:
+     SafeArea(
+      child: Stack(
+        alignment: Alignment.center,
+        overflow: Overflow.visible,
+        children: [
+          CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                expandedHeight: MediaQuery.of(context).size.height / 3,
+                actions: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: FlatButton.icon(
+                      label: Text('Sign Out', style: TextStyle(color:Colors.white),),
+                        icon: Icon(Icons.person, color: Colors.white,),
+                        onPressed: () async
     {
       await _auth.signOut();
-    }
-          , icon: Icon(Icons.person), label: Text('Sign Out'))
-        ],
-      ),
-      body: Column(
-        children: <Widget>[
-          Stack(
-            alignment: Alignment.center,
-            overflow: Overflow.visible,
-            children: [
-              Image(
-                  image: AssetImage('assets/images/pastor-chris.jpg'),
-                  height: MediaQuery.of(context).size.height / 3,
-                  width: double.infinity,
-                  fit: BoxFit.cover),
-              Positioned(
-                bottom: -70,
-                child: CircleAvatar(
-                  radius: 80,
-                  backgroundColor: Color(0xFF398AE5), //Colors.deepPurpleAccent[400],
-                  child: CircleAvatar(
-                    radius: 72,
-                    backgroundImage: AssetImage('assets/images/CHARLES_BROWN.jpg'),
-                  ),
+    }),
+                  )
+                ],
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Image.asset('assets/images/Pst_Chris.jpg',
+                      fit: BoxFit.cover),
                 ),
               ),
+              SliverList(
+                  delegate: SliverChildListDelegate([
+                    SizedBox(height:40.0),
+                ListTile(
+                  title: Center(
+                    
+                      child:
+                          Text('CHARLES BROWN', style: TextStyle(fontSize: 20, backgroundColor: Colors.blueGrey[100])),
+                    
+                  ),
+                ),
+                
+                   Padding(
+                     padding: const EdgeInsets.all(5.0),
+                     child: MenuLayout(),
+                   ),
+                
+              ]))
             ],
           ),
-          ListTile(
-              title: Center(
-                child: Padding(
-                  padding: const EdgeInsets.only(top:75.0),
-                  child: Text(
-                    'CHARLES BROWN',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
+          Positioned(
+            top: 115,
+            child: CircleAvatar(
+              radius: 80,
+              backgroundColor:
+                  Color(0xFF398AE5), //Colors.deepPurpleAccent[400],
+              child: CircleAvatar(
+                radius: 72,
+                backgroundImage: AssetImage('assets/images/CHARLES_BROWN.jpg'),
               ),
             ),
-         
-          // FlatButton.icon(onPressed: null, icon: null, label: Text('Rhapsody'),)
-         Expanded(child: MenuLayout() ) 
+          ),
         ],
       ),
-    );
+    ),);
   }
 }
