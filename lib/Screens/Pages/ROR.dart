bc3d07915212a250aa00b0149dd920496aef1fab
CharlesTheProgrammer <charlie.tbrown7@gmail.com>
2020-07-22 22:00:35 +0300

    calendar feature in ROR page

--- lib/Screens/Pages/ROR.dart
+++ lib/Screens/Pages/ROR.dart
@@ -1,64 +1,101 @@
 import 'package:flutter/material.dart';
-import 'package:spiritometer/UI/MenuUI.dart';
+import 'package:flutter_calendar_carousel/classes/event.dart';
+import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
+import 'package:spiritometer/Screens/Pages/rorRecord.dart';
+
 
 class RoR extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Stack(
-        alignment: Alignment.center,
-        overflow: Overflow.visible,
-        children: [
-          CustomScrollView(
-            slivers: <Widget>[
-              SliverAppBar(
-                expandedHeight: MediaQuery.of(context).size.height / 3,
-                actions: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: IconButton(
-                        icon: Icon(Icons.settings, color: Colors.white),
-                        onPressed: null ),
-                  )
-                ],
-                flexibleSpace: FlexibleSpaceBar(
-                  background: Image.asset('assets/images/Pst_Chris.jpg',
-                      fit: BoxFit.cover),
+    return Scaffold(
+      body: SafeArea(
+        left: true,
+        right: true,
+        bottom: true,
+        top: true,
+        child: Container(
+          // decoration: boxDecoration,
+          color: Colors.white54,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 14.0),
+                child: Row(
+                  children: [
+                    BackButton(color: Colors.black87),
+                    Spacer(),
+                    IconButton(
+                        icon: Icon(
+                          Icons.settings,
+                          color: Colors.black87,
+                        ),
+                        onPressed: null)
+                  ],
                 ),
               ),
-              SliverList(
-                  delegate: SliverChildListDelegate([
-                    SizedBox(height:40.0),
-                ListTile(
-                  title: Center(
-                    
-                      child:
-                          Text('CHARLES BROWN', style: TextStyle(fontSize: 20, backgroundColor: Colors.blueGrey[100])),
-                    
+              //SizedBox(height: 10),
+              Container(
+                //decoration: boxDecoration,
+                //color: Colors.blue,
+                margin: EdgeInsets.symmetric(horizontal: 16.0),
+                child: CalendarCarousel<Event>(
+                  onDayPressed: (DateTime date, List<Event> events) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => RorRecord(),
+                      ),
+                    );
+                  },
+                  weekendTextStyle: TextStyle(
+                    color: Colors.red,
                   ),
+                  thisMonthDayBorderColor: Colors.transparent,
+
+//      weekDays: null, /// for pass null when you do not want to render weekDays
+//      headerText: Container( /// Example for rendering custom header
+//        child: Text('Custom Header'),
+//      ),
+/*
+                  customDayBuilder: (
+                    /// you can provide your own build function
+                    /// to make custom day containers
+                    bool isSelectable,
+                    int index,
+                    bool isSelectedDay,
+                    bool isToday,
+                    bool isPrevMonthDay,
+                    TextStyle textStyle,
+                    bool isNextMonthDay,
+                    bool isThisMonthDay,
+                    DateTime day,
+                  ) {
+/// If you return null, [CalendarCarousel] will build container for current [day] 
+/// with default function.
+/// This way you can build custom containers for specific days only, leaving rest
+///  as default.
+// Example: every 15th of month, we have a flight, we can place an icon in the 
+/// container like that:
+                    if (day.day == DateTime.sunday) {
+                      return Center(
+                        child: Icon(Icons.hotel),
+                      );
+                      }else {
+                        return null;
+                      }
+                      
+                  },*/
+                  weekFormat: false,
+                  markedDatesMap: null,
+                  height: 400,
+                  selectedDateTime: DateTime.now(),
+                  
+                  daysHaveCircularBorder: true,
                 ),
-                
-                   Padding(
-                     padding: const EdgeInsets.all(5.0),
-                     child: MenuLayout(),
-                   ),
-                
-              ]))
+              ),
             ],
           ),
-          Positioned(
-            top: 115,
-            child: CircleAvatar(
-              radius: 80,
-              backgroundColor:
-                  Color(0xFF398AE5), //Colors.deepPurpleAccent[400],
-              child: CircleAvatar(
-                radius: 72,
-                backgroundImage: AssetImage('assets/images/CHARLES_BROWN.jpg'),
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
