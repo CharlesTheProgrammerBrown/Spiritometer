e9573428e237d95f0c42422accef09ceff5dd9bb
CharlesTheProgrammer <charlie.tbrown7@gmail.com>
2020-07-22 21:58:00 +0300

    Linear gradient theme added

--- lib/Screens/Pages/PrayNow.dart
+++ lib/Screens/Pages/PrayNow.dart
@@ -56,7 +56,21 @@ class _PrayNowState extends State<PrayNow> {
       color: Colors.transparent,
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.blue[500],
+  gradient: LinearGradient(
+    colors: <Color>[
+      Color(0xFF59c173),
+      Color(0xFF8f94fb),
+      Color(0xFFa17fe0),
+      
+    ],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    stops: [0.1, 0.7, 0.9],
+
+    
+  ),
+
+         
           borderRadius: BorderRadius.only(
             topLeft: Radius.circular(35.0),
             topRight: Radius.circular(35.0),
