626a02787f5181ea93bbd27db05ff8e8c120d0eb
CharlesTheProgrammer <charlie.tbrown7@gmail.com>
2020-08-06 00:48:10 +0300

    streambuilder profile

--- lib/Screens/Pages/Profile.dart
+++ lib/Screens/Pages/Profile.dart
@@ -3,24 +3,25 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:outline_gradient_button/outline_gradient_button.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
-
 import 'package:spiritometer/Services/auth.dart';
 import 'package:image_picker/image_picker.dart';
 
-import 'package:firebase_auth/firebase_auth.dart';
-
+import 'package:spiritometer/models/userDataModel/firebaseUserDataRepository.dart';
+import 'package:spiritometer/models/userDataModel/userDataModel.dart';
+import 'package:spiritometer/models/userDataModel/userDataRepository.dart';
+//import 'package:provider/provider.dart';
+//import 'package:spiritometer/models/user.dart';
 
 class Profile extends StatefulWidget {
-  final Future userName;
-  Profile(name, {this.userName});
-
   @override
   _ProfileState createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
+  final UserDataRepository _userDataRepository = FirebaseUserDataRepository();
+
   dynamic defaultAvatar = 'https://via.placeholder.com/150';
- final AuthService _auth = AuthService();
+  final AuthService _auth = AuthService();
   //File pathFiles;
   //bool fileAdded = false;
 
@@ -37,8 +38,6 @@ class _ProfileState extends State<Profile> {
 
   @override
   Widget build(BuildContext context) {
-//final userName = Provider.of<User>(context);
-
     var editProfilePic = GestureDetector(
       onTap: () {
         getImage();
@@ -64,50 +63,7 @@ class _ProfileState extends State<Profile> {
       ),
     );
 
-    var profilePic =  Column(
-        children: <Widget>[
-          Container(
-            child: Stack(
-              alignment: Alignment.topCenter,
-              children: [
-                OutlineGradientButton(
-                  gradient: LinearGradient(
-                    colors: [
-                      Color(0xFF59c173),
-                      Color(0xFF8f94fb),
-                      Color(0xFFa17fe0),
-                    ],
-                    begin: Alignment(-1, -1),
-                    end: Alignment(2, 2),
-                  ),
-                  strokeWidth: 5,
-                  padding: EdgeInsets.all(8),
-                  radius: Radius.circular(70),
-                  //Colors.deepPurpleAccent[400],
-                  child: CircleAvatar(
-                    radius: 64,
-                    //height: 70,
-                    backgroundImage: _image == null
-                        ? NetworkImage(defaultAvatar)
-                        : FileImage(_image),
-                  ),
-                ),
-                Positioned(
-                  bottom: 5,
-                  right: 10,
-                  child: editProfilePic,
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 10),
-          Text(
-            widget.userName.toString(),
-            style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20),
-          )
-        ],
-      );
-    
+    //var profilePic =
 
     var navHeader = Row(
       children: <Widget>[
@@ -133,63 +89,102 @@ class _ProfileState extends State<Profile> {
       data: ThemeData.dark(),
       child: Scaffold(
         body: GestureDetector(
-          
-                onTap: () => FocusScope.of(context).unfocus(),
-        
-            child: ListView(
-              physics: AlwaysScrollableScrollPhysics(),
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(top:2.0),
-                  child: navHeader,
-                ),
-                SizedBox(height: 10),
-                FutureBuilder(
-                  future: FirebaseAuth.instance.currentUser(),
-                  builder: (context,AsyncSnapshot<FirebaseUser> snapshot) {
-                    if(snapshot.hasData){
-                    return profilePic;
-                  }
-                  else{
-                    return Text('Loading');
-                  }
-                   },
-                    ),
-                SizedBox(height: 10),
-                      Container(
-
-                       // color: Colors.blue,
-                        height: MediaQuery.of(context).size.height*0.6,
-                        child:ListView(
-                          
-                          children:[ 
-                           SizedBox(height:30 ),
-                            ProfileListItem(
-                          icon: LineAwesomeIcons.user_edit,
-                          text: 'Change User Profile Name',
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: ListView(
+            physics: AlwaysScrollableScrollPhysics(),
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 2.0),
+                child: navHeader,
+              ),
+              SizedBox(height: 10),
+              StreamBuilder(
+                  stream: _userDataRepository.getCurrentUserData(),
+                  builder: (context, AsyncSnapshot<UserDataModel> snapshot) {
+                    return Column(
+                      children: <Widget>[
+                        Container(
+                          child: Stack(
+                            alignment: Alignment.topCenter,
+                            children: [
+                              OutlineGradientButton(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color(0xFF59c173),
+                                    Color(0xFF8f94fb),
+                                    Color(0xFFa17fe0),
+                                  ],
+                                  begin: Alignment(-1, -1),
+                                  end: Alignment(2, 2),
+                                ),
+                                strokeWidth: 5,
+                                padding: EdgeInsets.all(8),
+                                radius: Radius.circular(70),
+                                //Colors.deepPurpleAccent[400],
+                                child: snapshot.hasData
+                                    ? CircleAvatar(
+                                        radius: 64,
+                                        //height: 70,
+                                        backgroundImage:
+                                            snapshot.data.photoUrl != null
+                                                ? NetworkImage(
+                                                    snapshot.data.photoUrl)
+                                                : FileImage(_image),
+                                      )
+                                    : CircularProgressIndicator(),
+                              ),
+                              Positioned(
+                                bottom: 5,
+                                right: 10,
+                                child: editProfilePic,
+                              ),
+                            ],
+                          ),
                         ),
-                      //SizedBox(height:10 ),
-                      ProfileListItem(
-                        icon: LineAwesomeIcons.user_lock,
-                        text: 'Change Password',
-                      ),
-                     // SizedBox(height:10 ),
-                      ProfileListItem(
-                        icon: LineAwesomeIcons.bell,
-                        text: 'Notifications',
-                      ),
-                     // SizedBox(height:10 ),
-                      logOut,
-              ],),),],
-                  ),
-                
-                
-              
-            ),
+                        SizedBox(height: 10),
+                        snapshot.hasData
+                            ? Text(
+                                snapshot.data.name,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w400, fontSize: 20),
+                              )
+                            : Text("Profile Loading",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w400, fontSize: 20))
+                      ],
+                    );
+                  }),
+              SizedBox(height: 10),
+              Container(
+                // color: Colors.blue,
+                height: MediaQuery.of(context).size.height * 0.6,
+                child: ListView(
+                  children: [
+                    SizedBox(height: 30),
+                    ProfileListItem(
+                      icon: LineAwesomeIcons.user_edit,
+                      text: 'Change User Profile Name',
+                    ),
+                    //SizedBox(height:10 ),
+                    ProfileListItem(
+                      icon: LineAwesomeIcons.user_lock,
+                      text: 'Change Password',
+                    ),
+                    // SizedBox(height:10 ),
+                    ProfileListItem(
+                      icon: LineAwesomeIcons.bell,
+                      text: 'Notifications',
+                    ),
+                    // SizedBox(height:10 ),
+                    logOut,
+                  ],
+                ),
+              ),
+            ],
           ),
-        );
-      
-    
+        ),
+      ),
+    );
   }
 }
 
@@ -206,7 +201,7 @@ class ProfileListItem extends StatelessWidget {
       height: 45,
       width: double.infinity,
       //width: 50,
-      margin: EdgeInsets.symmetric(horizontal: 20).copyWith( top: 35),
+      margin: EdgeInsets.symmetric(horizontal: 20).copyWith(top: 35),
       padding: EdgeInsets.symmetric(horizontal: 20),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
