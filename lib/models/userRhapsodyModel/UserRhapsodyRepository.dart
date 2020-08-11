e61f917aa5d1afacc93250406507538ac97ad543
CharlesTheProgrammer <charlie.tbrown7@gmail.com>
2020-08-12 00:49:49 +0300

    Rhapsody Repository abstract class

---
+++ lib/models/userRhapsodyModel/UserRhapsodyRepository.dart
@@ -0,0 +1,12 @@
+import '../userRhapsodyModel/UserRhapsodyModel.dart';
+
+abstract class UserRhapsodyRepository{
+
+Future <String> addUserRhapsodyData(UserRhapsodyModel userRhapsodyData);
+
+Future<void> deleteUserRhapsodyData(UserRhapsodyModel userRhapsodyData);
+
+Stream<List<UserRhapsodyModel>> getListOfUserRhapsodyData();
+
+Future <void> updateUserRhapsodyData(UserRhapsodyModel updateUserRhapsodyData);
+}
