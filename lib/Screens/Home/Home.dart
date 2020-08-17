d38b141ebfde5d486a006284e22e4f8ae2a41990
CharlesTheProgrammer <charlie.tbrown7@gmail.com>
2020-08-17 03:55:26 +0300

    profile positioning change

--- lib/Screens/Home/Home.dart
+++ lib/Screens/Home/Home.dart
@@ -7,7 +7,7 @@ import 'package:spiritometer/models/userDataModel/userDataModel.dart';
 import 'package:spiritometer/models/userDataModel/userDataRepository.dart';
 
 class Home extends StatelessWidget {
- final UserDataRepository _userDataRepository = FirebaseUserDataRepository();
+  final UserDataRepository _userDataRepository = FirebaseUserDataRepository();
   @override
   Widget build(BuildContext context) {
     return Theme(
@@ -15,101 +15,100 @@ class Home extends StatelessWidget {
       child: Scaffold(
         body: SafeArea(
           child: StreamBuilder(
-            stream: _userDataRepository.getCurrentUserData(),
-            builder: ( context, AsyncSnapshot<UserDataModel> snapshot) {
-              return Stack(
-                alignment: Alignment.center,
-                overflow: Overflow.visible,
-                children: [
-                  CustomScrollView(
-                    slivers: <Widget>[
-                      SliverAppBar(
-                        automaticallyImplyLeading: false,
-                        expandedHeight: MediaQuery.of(context).size.height / 3,
-                        actions: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.only(
-                              top: 8.0,
-                            ),
-                            child: FlatButton.icon(
-                              label: Text(
-                                'Profile',
-                                style: TextStyle(
+              stream: _userDataRepository.getCurrentUserData(),
+              builder: (context, AsyncSnapshot<UserDataModel> snapshot) {
+                return Stack(
+                  alignment: Alignment.center,
+                  overflow: Overflow.visible,
+                  children: [
+                    CustomScrollView(
+                      slivers: <Widget>[
+                        SliverAppBar(
+                          automaticallyImplyLeading: false,
+                          expandedHeight:
+                              MediaQuery.of(context).size.height / 3,
+                          actions: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                top: 8.0,
+                              ),
+                              child: FlatButton.icon(
+                                label: Text(
+                                  'Profile',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                icon: Icon(
+                                  Icons.settings,
                                   color: Colors.white,
                                 ),
+                                onPressed: () {
+                                  Navigator.pushNamed(
+                                    context,
+                                    '/profilePage',
+                                  );
+                                  //async
+                                  //await _auth.signOut();
+                                },
                               ),
-                              icon: Icon(
-                                Icons.settings,
-                                color: Colors.white,
-                              ),
-                              onPressed: () {
-                                Navigator.pushNamed(
-                                  context,
-                                  '/profilePage',
-                                );
-                                //async
-                                //await _auth.signOut();
-                              },
+                            )
+                          ],
+                          flexibleSpace: FlexibleSpaceBar(
+                            background: Image.asset(
+                              'assets/images/Pst_Chris.jpg',
+                              fit: BoxFit.cover,
                             ),
-                          )
-                        ],
-                        flexibleSpace: FlexibleSpaceBar(
-                          background: Image.asset(
-                            'assets/images/Pst_Chris.jpg',
-                            fit: BoxFit.cover,
                           ),
                         ),
-                      ),
-                      SliverList(
-                        delegate: SliverChildListDelegate(
-                          [
-                            SizedBox(
-                              height: 90.0,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(
-                                5.0,
+                        SliverList(
+                          delegate: SliverChildListDelegate(
+                            [
+                              SizedBox(
+                                height: 90.0,
                               ),
-                              child: MenuLayout(),
-                            ),
-                          ],
+                              Padding(
+                                padding: const EdgeInsets.all(
+                                  5.0,
+                                ),
+                                child: MenuLayout(),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Positioned(
-                    top: 115,
-                    child: OutlineGradientButton(
-                      gradient: LinearGradient(
-                        colors: [
-                          Color(0xFF59c173),
-                          Color(0xFF8f94fb),
-                          Color(0xFFa17fe0),
-                        ],
-                        begin: Alignment(-1, -1),
-                        end: Alignment(2, 2),
-                      ),
-                      strokeWidth: 5,
-                      padding: EdgeInsets.all(8),
-                      radius: Radius.circular(90),
-                      //Colors.deepPurpleAccent[400],
-                      child: snapshot.hasData
-                                    ? CircleAvatar(
-                                        radius: 64,
-                                        //height: 70,
-                                        backgroundImage:
-                                            snapshot.data.photoUrl != null
-                                                ? NetworkImage(
-                                                    snapshot.data.photoUrl)
-                                                : AssetImage('assets/images/placeholder.png'),
-                                      )
-                                    : CircularProgressIndicator(),
+                      ],
                     ),
-                  ),
-                ],
-              );
-            }
-          ),
+                    Positioned(
+                      top: 150,
+                      child: OutlineGradientButton(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xFF59c173),
+                            Color(0xFF8f94fb),
+                            Color(0xFFa17fe0),
+                          ],
+                          begin: Alignment(-1, -1),
+                          end: Alignment(2, 2),
+                        ),
+                        strokeWidth: 7,
+                        padding: EdgeInsets.all(8),
+                        radius: Radius.circular(90),
+                        //Colors.deepPurpleAccent[400],
+                        child: snapshot.hasData
+                            ? CircleAvatar(
+                                radius: 64,
+                                //height: 70,
+                                backgroundImage: snapshot.data.photoUrl != null
+                                    ? NetworkImage(snapshot.data.photoUrl)
+                                    : AssetImage(
+                                        'assets/images/placeholder.png'),
+                              )
+                            : CircularProgressIndicator(),
+                      ),
+                    ),
+                  ],
+                );
+              }),
         ),
       ),
     );
