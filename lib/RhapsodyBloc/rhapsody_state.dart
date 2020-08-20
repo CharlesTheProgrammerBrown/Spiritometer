c01ab5c005fd96907d07ee316675d4a8e838e967
CharlesTheProgrammer <charlie.tbrown7@gmail.com>
2020-08-20 16:25:36 +0300

    Rhapsody state for bloc

--- lib/RhapsodyBloc/rhapsody_state.dart
+++ lib/RhapsodyBloc/rhapsody_state.dart
@@ -2,9 +2,35 @@ part of 'rhapsody_bloc.dart';
 
 abstract class RhapsodyState extends Equatable {
   const RhapsodyState();
+
+  List<Object> get props => [];
 }
 
-class RhapsodyInitial extends RhapsodyState {
+class RhapsodyInitial extends RhapsodyState {}
+
+class RhapsodyLoading extends RhapsodyState {}
+
+class RhapsodyLoaded extends RhapsodyState {
+  final List<UserRhapsodyModel> userRhapsodyModel;
+  final EventList<Event> markedDateMap;
+
+  RhapsodyLoaded({this.userRhapsodyModel, this.markedDateMap});
+
   @override
-  List<Object> get props => [];
+  List<Object> get props => [
+        userRhapsodyModel,
+        markedDateMap,
+      ];
+}
+
+class RhapsodyLoadFailed extends RhapsodyState {}
+
+class RhapsodySaveFailed extends RhapsodyState {
+  final String errorMessage;
+
+  RhapsodySaveFailed(this.errorMessage);
+  @override
+  List<Object> get props => [
+        errorMessage,
+      ];
 }
