4da2a8fa037077aa4f0f8b8aaf8e1129253d87b8
CharlesTheProgrammer <charlie.tbrown7@gmail.com>
2020-08-20 16:25:10 +0300

    Rhapsody Events for Bloc

--- lib/RhapsodyBloc/rhapsody_event.dart
+++ lib/RhapsodyBloc/rhapsody_event.dart
@@ -2,4 +2,30 @@ part of 'rhapsody_bloc.dart';
 
 abstract class RhapsodyEvent extends Equatable {
   const RhapsodyEvent();
+  @override
+  List<Object> get props => [];
+}
+
+class RhapsodyEntryLoadEvent extends RhapsodyEvent {}
+
+class RhapsodyEntryUpdated extends RhapsodyEvent {
+  final List<UserRhapsodyModel> rhapsodies;
+
+  RhapsodyEntryUpdated(this.rhapsodies);
+
+  @override
+  List<Object> get props => [rhapsodies];
+}
+
+class RhapsodySavedEvent extends RhapsodyEvent {
+  final String title;
+  final String note;
+
+  RhapsodySavedEvent({this.title, this.note});
+
+  @override
+  List<Object> get props => [
+        title,
+        note,
+      ];
 }
