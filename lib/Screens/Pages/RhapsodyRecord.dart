ff778e57475767e0d95f3c02f580057573d889b2
CharlesTheProgrammer <charlie.tbrown7@gmail.com>
2020-08-27 17:05:10 +0300

    Rhapsody Record page styling changes

--- lib/Screens/Pages/RhapsodyRecord.dart
+++ lib/Screens/Pages/RhapsodyRecord.dart
@@ -21,7 +21,6 @@ class _RhapsodyRecordState extends State<RhapsodyRecord> {
   final noteController = TextEditingController();
   DateTime _eventDate;
   final _formKey = GlobalKey<FormState>();
- 
 
   bool showProgressIndicator = true;
   bool processing;
@@ -30,8 +29,8 @@ class _RhapsodyRecordState extends State<RhapsodyRecord> {
   void initState() {
     super.initState();
 
-processing = false;
-_eventDate = DateTime.now();
+    processing = false;
+    _eventDate = DateTime.now();
     titleController.addListener(_updateTitleValue);
     noteController.addListener(_updateNoteValue);
   }
@@ -86,170 +85,176 @@ _eventDate = DateTime.now();
 
   @override
   Widget build(BuildContext contextB) {
+    final double width = MediaQuery.of(context).size.width / 2;
     return WillPopScope(
-      child: Theme(
-        data: Theme.of(contextB)
-            .copyWith(scaffoldBackgroundColor: Colors.grey[300]),
-        child: Scaffold(
-          body: Builder(
-            builder: (context) {
-              return  Column(
-                    
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 14.0),
-                        child: Row(
-                          children: [
-                            BackButton(color: Colors.black45),
-                          ],
-                        ),
-                      ),
+      child: Scaffold(
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'RHAPSODY STUDY',
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 18,
+              letterSpacing: 1,
+              color: Colors.white,
+              decorationStyle: TextDecorationStyle.solid,
 
-                      //2nd element
-                      Center(
-                        child: Text(
-                          'RHAPSODY',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              fontSize: 18,
-                              letterSpacing: 1,
-                              color: Colors.white,
-                              decorationStyle: TextDecorationStyle.solid,
-                              decoration: TextDecoration.underline,
-                              decorationColor: Colors.deepPurple),
-                        ),
-                      ),
-                      SizedBox(height: 10),
+              // decorationColor: Colors.deepPurple),
+            ),
+          ),
+          backgroundColor: Color(0xFF8f94fb),
+        ),
+        body: Builder(
+          builder: (context) {
+            return Column(
+              children: [
+                //2nd element
 
-                      Form(
-                        key: _formKey,
-                                              child: Container(
-                          alignment: Alignment.center,
-                          child: ListView(
-                            // mainAxisAlignment: MainAxisAlignment.center,
-                            //crossAxisAlignment: CrossAxisAlignment.start,
-                            shrinkWrap: true,
-                            children: <Widget>[
-                               Padding(
-                                     padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                                    child: TextFormField(
-                                      controller: titleController,
-                                      validator: (value) =>
+                SizedBox(height: 10),
+
+                Form(
+                  key: _formKey,
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: ListView(
+                      // mainAxisAlignment: MainAxisAlignment.center,
+                      //crossAxisAlignment: CrossAxisAlignment.start,
+                      shrinkWrap: true,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 15.0),
+                          child: Container(
+                            color: Colors.white,
+                            child: TextFormField(
+                              controller: titleController,
+                              validator: (value) =>
                                   (value.isEmpty) ? "Please Enter title" : null,
-                                      textCapitalization:
-                                          TextCapitalization.characters,
-                                      autofocus: true,
-                                      autocorrect: true,
-                                      onTap: () {},
-                                      cursorColor: Colors.black,
-                                      style: rorRecordTextStyle,
-                                      decoration: InputDecoration(
-                                        border: InputBorder.none,
-                                        contentPadding: EdgeInsets.only(top: 14.0),
-                                        hintText: 'Add Title',
-                                        hintStyle: rorRecordTextStyle,
-                                        //fillColor: Colors.white,
-                                        //filled: true
-                                      ),
-                                    ),
-                                  ),
-                                
-                              
-                               Padding(
-                                  padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                                  child: TextFormField(
-                                    controller: noteController,
-                                    validator: (value) =>
-                            (value.isEmpty) ? "Please Enter Note" : null,
-                                    textCapitalization: TextCapitalization.sentences,
-                                    keyboardType: TextInputType.multiline,
-                                    textAlignVertical: TextAlignVertical.top,
-                                    cursorColor: Colors.black,
-                                    style: rorRecordTextStyle,
-                                    textInputAction: TextInputAction.newline,
-                                    maxLines: null,
-                                    decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      contentPadding: EdgeInsets.only(top: 14.0),
-                                      hintText: 'Note',
-                                      hintStyle: rorRecordTextStyle,
-                                    ),
-                                  ),
-                                ),
-                              
- const SizedBox(height: 10.0),
-              ListTile(
-                title: Text("Date (YYYY-MM-DD)"),
-                subtitle: Text("${_eventDate.year} - ${_eventDate.month} - ${_eventDate.day}"),
-                onTap: ()async{
-                  DateTime picked = await showDatePicker(context: context, initialDate: _eventDate, firstDate: DateTime(_eventDate.year-5), lastDate: DateTime(_eventDate.year+5));
-                  if(picked != null) {
-                    setState(() {
-                          _eventDate = picked;
-                    });
-                  }
-                },
-              ),
-              SizedBox(height: 10.0),
-              processing
-                  ? Center(child: CircularProgressIndicator())
-                  : Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                              
+                              textCapitalization: TextCapitalization.characters,
+                              autofocus: true,
+                              autocorrect: true,
+                              onTap: () {},
+                              maxLines: 1,
+                              cursorColor: Colors.black,
+                              style: rorRecordTextStyle,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                contentPadding:
+                                    EdgeInsets.only(top: 14.0, left: 10.0),
+                                hintText: 'Add Title',
+                                hintStyle: rorRecordTextStyle,
+                                //fillColor: Colors.white,
+                                //filled: true
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 15.0),
+                          child: Container(
+                            color: Colors.white,
+                            child: TextFormField(
+                              controller: noteController,
+                              validator: (value) =>
+                                  (value.isEmpty) ? "Please Enter Note" : null,
+                              textCapitalization: TextCapitalization.sentences,
+                              keyboardType: TextInputType.multiline,
+                              textAlignVertical: TextAlignVertical.top,
+                              cursorColor: Colors.black,
+                              style: rorRecordTextStyle,
+                              textInputAction: TextInputAction.newline,
+                              minLines: 5,
+                              maxLines: null,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                contentPadding:
+                                    EdgeInsets.only(top: 14.0, left: 10.0),
+                                hintText: 'Note',
+                                hintStyle: rorRecordTextStyle,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 10.0),
+                        Card(
+                          color: Color(0xFF8f94fb),
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 20),
+                          child: ListTile(
+                            title: Text(
+                              "Pick Date (YYYY-MM-DD) \n ",
+                              textAlign: TextAlign.center,
+                            ),
+                            subtitle: Text(
+                              "Current Date: ${_eventDate.year} - ${_eventDate.month} - ${_eventDate.day}",
+                              style: TextStyle(fontSize: 16),
+                              textAlign: TextAlign.justify,
+                            ),
+                            onTap: () async {
+                              DateTime picked = await showDatePicker(
+                                  context: context,
+                                  initialDate: _eventDate,
+                                  firstDate: DateTime(_eventDate.year - 5),
+                                  lastDate: DateTime(_eventDate.year + 5));
+                              if (picked != null) {
+                                setState(() {
+                                  _eventDate = picked;
+                                });
+                              }
+                            },
+                          ),
+                        ),
+                        SizedBox(height: 10.0),
+                        processing
+                            ? Center(child: CircularProgressIndicator())
+                            : Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 16.0),
                                 child: FlatButton.icon(
                                   shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(8.0),
+                                    borderRadius: BorderRadius.circular(16.0),
                                   ),
-                                  color: Colors.blue[400],
+                                  color: Colors.amberAccent,
                                   onPressed: () async {
                                     if (_formKey.currentState.validate()) {
                                       processing = true;
                                       await _firebaseUserRhapsodyDataRepository
                                           .addUserRhapsodyData(
                                         UserRhapsodyModel(
-                                          title: titleController.text,
-                                          note: noteController.text,
-                                          eventDate: _eventDate
-
-                                        ),
+                                            title: titleController.text,
+                                            note: noteController.text,
+                                            eventDate: _eventDate),
                                       );
-                                    
-                                         
-                                          Navigator.pop(context);
-                                  setState(() {
-                                    processing = false;
-                                 
-                                                                  });
-                                                                        
-                                                                    }
-                                                                  },
-                                                                  icon: Icon(
-                                                                    Icons.save,
-                                                                    size: 15,
-                                                                  ),
-                                                                  label: Text(
-                                                                    'Save',
-                                                                  ),
-                                                                ),
-                                                              ),
-                                                            ],
-                                                          ),
-                        ),
-                      ),
-                                                    ],
-                                                  
-                                                
-                                              );
-                                            },
-                                          ),
-                                        ),
-                                      ),
-                                      onWillPop: () => onWillPop(
-                                        contextB,
-                                      ),
-                                    );
-                                  }
-                                   
-                                }
-                                
-                               
+
+                                      Navigator.pop(context);
+                                      setState(() {
+                                        processing = false;
+                                      });
+                                    }
+                                  },
+                                  icon: Icon(
+                                    Icons.save,
+                                    size: 15,
+                                  ),
+                                  label: Text(
+                                    'Save',
+                                  ),
+                                ),
+                              ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+      onWillPop: () => onWillPop(
+        contextB,
+      ),
+    );
+  }
+}
